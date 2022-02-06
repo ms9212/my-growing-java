5967d00d2c362bddb35a866ded3064594d75abc0
ms9212 <perso07.ms@gmail.com>
2022-02-06 14:21:20 +0900

    [ADD] 새 패키지, Hello World 클래스

---
+++ java-grammar/src/e001/hello_world/HelloWorld.java
@@ -0,0 +1,12 @@
+package e001.hello_world;
+
+/**
+ * 문서화 주석
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 여기에 쓰세요.
+        // 여기가 실행되는 곳이에요.
+        System.out.println("HelloWorld");
+    }
+}
